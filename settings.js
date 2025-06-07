a0e1ea80c83d12b388701534b1e4e11ed3f00080
Darkyyy <127139301+Fengqin89389@users.noreply.github.com>
2025-06-08 00:08:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Darkyyy', // Your name
-  ownerNumber: '919681125468', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'Jeffri', // Your name
+  ownerNumber: '919606720805', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
